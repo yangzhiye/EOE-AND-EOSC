b65c1997ec644ec862b4f67ef92d589a532423dd
Zhiye Yang <445922703@qq.com>
2016-12-14 21:44:55 +0800

    update get_esc_data

--- get_data.py
+++ get_data.py
@@ -1,6 +1,5 @@
 import numpy as np
 from gensim.models import Word2Vec
-from keras.utils import np_utils
 
 
 MAX_LEN = 79
@@ -9,7 +8,7 @@ sentences_train_X = []
 sentences_test_X = []
 sentences_train_Y = []
 sentences_test_Y = []
-model = Word2Vec.load("./model/50features_1minwords_10context")
+w2cmodel = Word2Vec.load("./model/50features_1minwords_10context")
 POS_DIC = {"X":0,"O":1,"B":2,"I":3}
 DIC_LEN = len(POS_DIC)
 
@@ -17,6 +16,7 @@ def get_basic_data(sentences_X,sentences_Y,filepath):
     f = open(filepath)
     list_X = []
     list_Y = []
+    list_esc = []
     for line in f.readlines():
         if(line.strip()=="<end_for_sentence>"):
             sentences_X.append(list_X)
@@ -26,9 +26,12 @@ def get_basic_data(sentences_X,sentences_Y,filepath):
         else:
             list_X.append(line.split(" ")[0].strip().lower())
             list_Y.append(line.split(" ")[1].strip())
+            if line.split(" ")[1].strip() == 'B':
+                list_esc.append(line.split(" ")[2].strip())
+    return list_esc
 
 
-def return_data():
+def return_eoa_data():
     get_basic_data(sentences_test_X,sentences_test_Y,'./data/test')
     get_basic_data(sentences_train_X,sentences_train_Y,'./data/train')
 
@@ -42,11 +45,11 @@ def return_data():
 
     for i,line in enumerate(sentences_train_X):
         for j,word in enumerate(line):
-            train_X[i][j] = model[word]
+            train_X[i][j] = w2cmodel[word]
             train_Y[i][j][POS_DIC[sentences_train_Y[i][j]]] = 1
     for i,line in enumerate(sentences_test_X):
         for j,word in enumerate(line):
-            test_X[i][j] = model[word]
+            test_X[i][j] = w2cmodel[word]
             test_Y[i][j][POS_DIC[sentences_test_Y[i][j]]] = 1
 
     for i in range(train_Y.shape[0]):
@@ -60,4 +63,117 @@ def return_data():
     return train_X,train_Y,test_X,test_Y
 
 
+def return_b_size(Y):
+    size = 0
+    for i in Y:
+        for j in i:
+            if j == 'B':
+                size+=1
+    return size
 
+
+def return_max_sc(test_Y,train_Y):
+    max_sc = 0
+    for i,line in enumerate(test_Y):
+        for j,word in enumerate(line):
+            if word == 'B':
+                if max_sc < max(j+1,len(line)-j+1):
+                    max_sc = max(j+1,len(line)-j+1)
+    for i,line in enumerate(train_Y):
+        for j,word in enumerate(line):
+            if word == 'B':
+                if max_sc < max(j+1,len(line)-j+1):
+                    max_sc = max(j+1,len(line)-j+1)
+    return max_sc
+
+
+def get_esc_data(): # evaluation sentiment classification
+    SC = 4 #0,1,2,3
+    list_test = get_basic_data(sentences_test_X,sentences_test_Y,'./data/test')
+    list_train = get_basic_data(sentences_train_X,sentences_train_Y,'./data/train')
+    B_SIZE_TRAIN = return_b_size(sentences_train_Y) #3329
+    B_SIZE_TEST = return_b_size(sentences_test_Y) #364
+    #MAX_SC = return_max_sc(sentences_test_Y,sentences_train_Y) #73 maybe no use.
+    train_X_F = np.zeros((B_SIZE_TRAIN, MAX_LEN, 2 * WORD_DIM))
+    train_X_B = np.zeros((B_SIZE_TRAIN, MAX_LEN, 2 * WORD_DIM))
+    train_Y = np.zeros((B_SIZE_TRAIN, SC))
+    test_X_F = np.zeros((B_SIZE_TEST, MAX_LEN, 2 * WORD_DIM))
+    test_X_B = np.zeros((B_SIZE_TEST, MAX_LEN, 2 * WORD_DIM))
+    test_Y = np.zeros((B_SIZE_TEST, SC))
+    count = -1
+
+    for i,line in enumerate(sentences_train_Y):
+        for j,word in enumerate(line):
+            if word == 'B':
+                count += 1
+                # get evaluation object vector
+                i_count = 0
+                while j+1 < len(line) and sentences_train_Y[i][j+i_count+1] == 'I':
+                    i_count += 1
+                temp_v = w2cmodel[sentences_train_X[i][j]]
+                p_i_count = i_count
+                while i_count > 0:
+                    temp_v += w2cmodel[sentences_train_X[i][j+i_count]]
+                    i_count -= 1
+                if p_i_count > 0:
+                    temp_v /= (p_i_count+1)
+
+                # get this F and B data
+                temp_data = np.zeros(WORD_DIM*2)
+                for k , word in enumerate(sentences_train_X[i]):
+                    if k < j :
+                        train_X_F[count][k][:50] = w2cmodel[sentences_train_X[i][k]]
+                        train_X_F[count][k][50:] = temp_v
+                    if k == j or p_i_count > 0:
+                        train_X_F[count][k][:50] = w2cmodel[sentences_train_X[i][k]]
+                        train_X_F[count][k][50:] = temp_v
+                        train_X_B[count][k-j][:50] = w2cmodel[sentences_train_X[i][k]]
+                        train_X_B[count][k-j][50:] = temp_v
+                        if k > j:
+                            p_i_count -= 1
+                    if k > j:
+                        train_X_B[count][k-j][:50] = w2cmodel[sentences_train_X[i][k]]
+                        train_X_B[count][k-j][50:] = temp_v
+    count = -1
+    for i, line in enumerate(sentences_test_Y):
+        for j, word in enumerate(line):
+            if word == 'B':
+                count += 1
+                # get evaluation object vector
+                i_count = 0
+                while j + 1 < len(line) and sentences_test_Y[i][j + i_count + 1] == 'I':
+                    i_count += 1
+                temp_v = w2cmodel[sentences_test_X[i][j]]
+                p_i_count = i_count
+                while i_count > 0:
+                    temp_v += w2cmodel[sentences_test_X[i][j + i_count]]
+                    i_count -= 1
+                if p_i_count > 0:
+                    temp_v /= (p_i_count + 1)
+
+                # get this F and B data
+                temp_data = np.zeros(WORD_DIM * 2)
+                for k, word in enumerate(sentences_test_X[i]):
+                    if k < j:
+                        test_X_F[count][k][:50] = w2cmodel[sentences_test_X[i][k]]
+                        test_X_F[count][k][50:] = temp_v
+                    if k == j or p_i_count > 0:
+                        test_X_F[count][k][:50] = w2cmodel[sentences_test_X[i][k]]
+                        test_X_F[count][k][50:] = temp_v
+                        test_X_B[count][k - j][:50] = w2cmodel[sentences_test_X[i][k]]
+                        test_X_B[count][k - j][50:] = temp_v
+                        if k > j:
+                            p_i_count -= 1
+                    if k > j:
+                        test_X_B[count][k - j][:50] = w2cmodel[sentences_test_X[i][k]]
+                        test_X_B[count][k - j][50:] = temp_v
+
+    for i,line in enumerate(train_Y):
+        line[list_train[i]] = 1
+    for i,line in enumerate(test_Y):
+        line[list_test[i]] = 1
+
+    return train_X_F,train_X_B,train_Y,test_X_F,test_X_B,test_Y
+
+if __name__ == "__main__":
+    get_esc_data()
--- test_model.py
+++ test_model.py
@@ -1,11 +1,10 @@
 from keras.models import load_model
 import numpy as np
 import get_data
-
 MAX_LEN = 79
 
 
-def my_eva(test_Y,predict_test_Y):
+def eoa_eva(test_Y,predict_test_Y):
     total = 0
     right = 0
     total_O = 0
@@ -17,7 +16,7 @@ def my_eva(test_Y,predict_test_Y):
     for i in range(test_Y.shape[0]):
         for j in range(MAX_LEN):
             if test_Y[i][j][0] == 1:
-                break
+                continue
             total += 1
             if test_Y[i][j][1] == 1:
                 total_O += 1
@@ -39,8 +38,7 @@ def my_eva(test_Y,predict_test_Y):
     print "B acc is %f" % (right_B * 1.0 / total_B)
     print "I acc is %f" % (right_I * 1.0 / total_I)
 
-
-def my_eva1(test_Y,predict_test_Y): #(304,79,4),(304,79) {"X":0,"O":1,"B":2,"I":3}
+def eoa_eva1(test_Y,predict_test_Y): #(304,79,4),(304,79) {"X":0,"O":1,"B":2,"I":3}
     total = 0
     right = 0
     for i in range(test_Y.shape[0]):
@@ -67,14 +65,19 @@ def my_eva1(test_Y,predict_test_Y): #(304,79,4),(304,79) {"X":0,"O":1,"B":2,"I":
 
     print "eva1 is %f" % (right*1.0/total)
 
-if __name__ == "__main__":
-    train_X,train_Y,test_X,test_Y = get_data.return_data()
+
+def test_eoa():
+    train_X,train_Y,test_X,test_Y = get_data.return_eoa_data()
     lstmmodel = load_model("./model/lstm_model")
     #print lstmmodel.metrics_names
     #print lstmmodel.evaluate(test_X,test_Y)
     predict_test_Y = lstmmodel.predict_classes(test_X)
-    my_eva(test_Y, predict_test_Y)
-    my_eva1(test_Y, predict_test_Y)
+    eoa_eva(test_Y, predict_test_Y)
+    eoa_eva1(test_Y, predict_test_Y)
+
+
+if __name__ == "__main__":
+    test_eoa()
 
 
 
